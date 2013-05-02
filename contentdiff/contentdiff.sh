4da6cb0619b501b33fcd091d9c8a32c6fb7efded
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-02 14:13:17 +0200

    small diff script, not even tested

---
+++ contentdiff/contentdiff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+usage()
+{
+    echo "test: list all files; filter filenames w/o paths; sort alphabetical and compare"
+    echo "$0 <folder A> <folder B>"
+    echo
+    exit 1
+}
+[[ -z $1 ]] && usage
+[[ -z $2 ]] && usage
+
+FOLDER_A=$1
+TMP_A="/tmp/con.a"
+FOLDER_B=$2
+TMP_B="/tmp/con.b"
+
+find ${FOLDER_A} -exec basename {} \; | sort > ${TMP_A}
+find ${FOLDER_B} -exec basename {} \; | sort > ${TMP_B}
+diff ${TMP_A} ${TMP_B}
