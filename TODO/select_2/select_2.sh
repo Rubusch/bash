4b3b53f7f8d9916e9fe9d4b08b29412c057d30ca
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-04 14:04:02 +0200

    select2: update comments

--- TODO/select_2/select_2.sh
+++ TODO/select_2/select_2.sh
@@ -9,19 +9,24 @@ echo "select demo"
 
 PS3="your choice: "
 QUIT="quit this program"
-touch "$QUIT"
 
-select FILENAME in *;
-do
+# select offers to select files
+#
+# for this example to show two possible choices
+# we make a touch on "quit this program"
+touch ${QUIT}
+
+select FILENAME in *; do
   case $FILENAME in
-      "$QUIT")
-	  echo "exiting"
+    "$QUIT") # if we choose the file "quit this program", this case statement will break and exit
+      echo "exiting"
 	  break
 	  ;;
-      *)
+    *)
 	  echo "you picked $FILENAME ($REPLY)"
 	  ;;
   esac
 done
 
+# don't forget to remove the "quit this program" file
 echo "READY."
