cb60eed444f641de270ad6e29eef155934bcf45d
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-04 13:52:30 +0200

    aliasing: fixed script

--- TODO/variables/TODO_aliasing.sh
+++ TODO/variables/TODO_aliasing.sh
@@ -2,16 +2,24 @@
 
 ## aliasing - TODO
 
+
 echo "1. we set up an alias: dh = 'df -h'"
-echo $(alias dh='df -h')
+alias dh='df -h' 
+
+## in shell script you need to expand aliases as shell script option: shopt
+shopt -s expand_aliases
+
 echo
 echo "2. we'll test the alias:"
-echo "$(dh)"
+dh
+
 echo
 echo "3. no we'll unalias again"
-echo "$(unalias dh)"
+unalias dh
+
 echo
 echo "4. we'll test the alias:"
-echo "$(dh)"
+dh
+
 echo
 echo "READY."
