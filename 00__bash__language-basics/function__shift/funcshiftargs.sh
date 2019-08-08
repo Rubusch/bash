15614e83537115bee0b62cd7acb4e55553171a0f
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-08 10:18:46 +0200

    function__shift: moved

--- 00__bash__language-basics/function__shift/funcshiftargs.sh
+++ 00__bash__language-basics/function__shift/funcshiftargs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ##
+## demostrates shift of function arguments
+##
 ## @author: Lothar Rubusch
 ## @email: L.Rubusch@gmx.ch
 ## @license: GPLv3
@@ -10,12 +12,18 @@ func()
 
     if [[ "$#" -gt 1 ]]; then
         local var=$1
+        echo "shift"
         shift
     fi
     local other_var=$1
+
+## now shift
+    echo "shift"
     shift
 
     echo "num, after '$#'"
+    echo
+
     echo "var '$var', leaves this empty if no 'var' was defined"
     echo "other var '$other_var'"
     echo
