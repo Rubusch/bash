afe8be5c77b821452fca0c1f0aad5985b7827122
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-12 11:48:27 +0200

    shift1: cleanup

--- TODO/shift_1/shift_1.sh
+++ TODO/shift_1/shift_1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 ##
 ## demonstrates the usage of shift
 ##
@@ -6,25 +6,23 @@
 USAGE="usage: $0 <arg1> <arg2> <arg3> ... <argN>"
 
 ## check if $# (list of command line arguments) was empty
-if [ "$#" == "0" ]; then
+if (( "$#" == "0" )); then
     echo "$USAGE"
     exit 1
 fi
 
-## 
 idx="1"
 while ((  "$#" )); do
     echo -n "$idx. "
-    if [[ "$1" == "asdf" ]]; then 
+    if [[ "$1" == "asdf" ]]; then
 	echo "don't do asdf!!!"
     else
 	echo "$1"
     fi
-    ## the shift shifts $1 to the next element in $# - so that the content  
+    ## the shift shifts $1 to the next element in $# - so that the content
     ## in the loops always only treats $1
     shift
     idx=$[$idx+1]
 done
 
 echo "READY."
-
