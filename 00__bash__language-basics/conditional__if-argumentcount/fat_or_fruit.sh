633b8cf82ef1f1ee6947649ff7523c405921980a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-22 11:16:39 +0200

    if-argumentcount: added

--- 00__bash__language-basics/conditional__if-argumentcount/fat_or_fruit.sh
+++ 00__bash__language-basics/conditional__if-argumentcount/fat_or_fruit.sh
@@ -1,29 +1,33 @@
-#!/bin/bash
+#!/bin/bash -e
 ##
 ## @author: Lothar Rubusch
 ## @email: L.Rubusch@gmx.ch
 ## @license: GPLv3
 ##
-## if-else-example
+## if-else-example, comparison of number of arguments with $#
+##
 ## uses arguments:
 ## ./fatorfruit.sh <weight> <height>
 
+
+## number comparison 
+##
 ## $# is the argument counter
-if [ ! $# == 2 ] ; then
+## in bash (!) comparison with $# should use '((' and '))'
+if (( $# < 2 )) ; then
     echo "usage: ./fatorfruit.sh <weight> <height>"
     exit 1;
 fi
 
+
+## demo code...
 weight="$1"
 height="$2"
-
 idealweight=$[height - 110]
-
-if [ $weight -le $idealweight ] ; then
+if [[ $weight -le $idealweight ]] ; then
     echo "you should eat a bit more fat"
 else
     echo "you should eat a bit more fruit"
 fi
-
 echo
 echo "READY."
