e66734b5259e074292e69c8c94c001745a1c077d
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-24 11:08:22 +0200

    conditional: another example, boolean

--- 00__bash__language-basics/conditional__boolean/leapyear_boolean.sh
+++ 00__bash__language-basics/conditional__boolean/leapyear_boolean.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 ##
 ## @author: Lothar Rubusch
 ## @email: L.Rubusch@gmx.ch
 ## @license: GPLv3
 ##
 ## leapyear_boolean.sh
-##
-##
 
 echo "checks for a leap year by a boolean check"
-
 year=`date +%Y`
-
 if (( ("$year" % 400) == "0" )) || (( ("$year" % 4 == "0") && ("$year" % 100 != "0") )); then
     echo "this is a leap year, february has 29 days"
 else
