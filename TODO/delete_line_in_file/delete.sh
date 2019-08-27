84a738baa8b9db7bea8fbaf031ba9da1650e1db4
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-27 09:57:23 +0200

    delete_line_in_file: comments

--- TODO/delete_line_in_file/delete.sh
+++ TODO/delete_line_in_file/delete.sh
@@ -11,6 +11,10 @@ cp ./test.txt ./test.txt.bk
 echo
 
 ## delete line in textfile
+##
+## ex -s: 'ex' is vim, '-s' is silent mode
+## run the two vim commands (replace), then 'wq' in silenced vim (ex-mode) on
+## file test.txt
 (echo "g/jack and/d"; echo 'wq') | ex -s ./test.txt
 
 ## or
