e9d43d681ed1ea5d89999084550cd6e2abc506e2
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-01 12:16:58 +0200

    ls: indented, update

--- TODO/indent_ls/indentls.sh
+++ TODO/indent_ls/indentls.sh
@@ -3,7 +3,15 @@
 ## @author: Lothar Rubusch
 ## @email: L.Rubusch@gmx.ch
 ## @license: GPLv3
-
+##
+## simple example of use case that appears from time to time,
+## - have a directory content listed in one column
+## - avoid further adornments to the files/folders
+## - have element in a separate eline (prepared for bash arrays)
+## => ls -1
+##
+## - present the content indented,
+## => sed...
 ls -1 | sed 's/^/  /'
 
 echo "READY."
