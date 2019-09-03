955cfb7989213fd964ed83dfaf381ef4d9cf0068
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-04 00:08:51 +0200

    rmsafely: update

--- TODO/lib_rmsafely/rmsafely.sh
+++ TODO/lib_rmsafely/rmsafely.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 ##
 ## @author: Lothar Rubusch
 ## @email: L.Rubusch@gmx.ch
@@ -16,19 +16,28 @@ rm_safely()
 {
     local target=$1
 
+## e.g. rm $VAR and VAR was empty
     if [[ -z "${target}" ]]; then
         echo "Nothing to remove here: '${target}'!"
         return
     fi
 
-    if [[ "${target}" = "/" ]]; then
+## e.g. $VAR/... and VAR was empty, results to /...
+    if [[ "${target}" == "/" ]]; then
         die "ERROR: Remove target was root: '${target}'!"
     fi
 
+## e.g. $VAR1/$VAR2/... and both are empty, results to //...
+	if [[ "${target}" =~ (/)+ ]]; then
+        die "ERROR: Remove target was root: '${target}'!"
+    fi
+
+## e.g. match fundamental system folders
     if [[ "${target}" =~ "^(/bin|/boot|/dev|/etc|/home|/lib|/mnt|/opt|/proc|/root|/sbin|/sys|/tmp|/usr|/var)/?$" ]]; then
         die "ERROR: Something is odd here, are you trying to remove the root file system: '${target}'?!"
     fi
 
+## removal
     if [[ -d "$target" ]]; then
         rm -rfv "${target}"
     else
@@ -36,3 +45,14 @@ rm_safely()
     fi
 }
 
+
+### START
+
+echo "execute 'rm -rf \$FOO/\$BAR/*'"
+echo
+
+set -x
+rm_safely $FOO/$BAR/*
+{ set +x ; } 2> /dev/null
+
+echo "READY."
