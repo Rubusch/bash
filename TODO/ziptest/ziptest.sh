e14c0c44a3375cf011ffd336573053c176f28c35
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-06 12:13:16 +0200

    zip: update zip script

--- TODO/ziptest/ziptest.sh
+++ TODO/ziptest/ziptest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 ## test runs over a list of file contents and zips them one by one
 
@@ -9,13 +9,11 @@ dir="./dir"
 ## !!! go into the folder - important for zip to work !!!
 cd ${dir}
 
-## generate files
+## setup list of files to zip
 touch ./foo1.txt
 touch ./foo2.txt
 touch ./foo3.txt
 touch ./foo4.txt
-
-## get list
 LIST=( `ls -1v` )
 
 ## zip each
