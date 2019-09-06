222d1e667e2dc5c1298eb0c6dbcd997792e2c2d7
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-06 11:51:00 +0200

    sortnumbers: moved and renamed

--- 00__bash__language-basics/array__sorting/sortnumbers.sh
+++ 00__bash__language-basics/array__sorting/sortnumbers.sh
@@ -2,17 +2,19 @@
 ##
 ## sorts some numbers from echo (array)
 
-echo "output"
+echo "array by 'echo output'"
 sorted=$(echo "03 53 50 88 01 32 28 74 95 06" | tr ' ' '\n' | sort -n -k 2 | paste -s -d' ' -)
+echo "sorted:"
 echo "${sorted[*]}"
 echo
 
-echo "array"
+echo "array by 'bash array'"
 arr=( "03" "53" "50" "88" "01" "32" "28" "74" "95" "06" )
 sorted=( $(printf "%s\n" ${arr[*]} | sort -n) )
-## or also
+## alternatively
 #sorted="${arr[*]}"
 #sorted=( $(printf "%s\n" ${sorted} | sort -n) )
+echo "sorted:"
 echo "${sorted[*]}"
 echo
 
