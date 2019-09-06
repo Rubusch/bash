f7d20797381b3eb934c638c21d2144a6db704c4a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-06 11:19:39 +0200

    sed: insert lines update

--- TODO/prepend_in_file/prepend.sh
+++ TODO/prepend_in_file/prepend.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/bin/bash -e
+##
+##
 
 echo "BEFORE"
 cat ./test.txt
-cp ./test.txt ./test.txt.bk
+cp ./test.txt /tmp/test.txt.bk
 echo
 
-## prepend
-sed -i '1i once upon a time' ./test.txt
+## prepend, 1st line i-nsert
+sed -i '1i once upon a time...' ./test.txt
 #sed -ie 's/ill/ock/' ./test.txt
 
-## insert second line
-sed -i '5i and the insurance didn`t pay anything!' ./test.txt
+## insert second line, at 5th line i-nsert
+sed -i '5i ...and the insurance didn`t pay anything!!!' ./test.txt
 
 #sed -i '2i if \[\[ \$\{CURR_SC\} == \"1\" \]\]; then' ./test.txt
 
 echo "AFTER"
 cat ./test.txt
-cp ./test.txt.bk ./test.txt
+mv /tmp/test.txt.bk ./test.txt
 echo
